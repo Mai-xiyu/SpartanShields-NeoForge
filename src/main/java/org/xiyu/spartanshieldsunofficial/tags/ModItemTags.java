85d69d8191cd4b18c142a245d1c9f42b7c9a7dc2
Mai-xiyu <maixiyumc@gmail.com>
2026-02-06 21:49:57 +0800

    fix Forge-> NeoForge Tags ERROR

--- src/main/java/org/xiyu/spartanshieldsunofficial/tags/ModItemTags.java
+++ src/main/java/org/xiyu/spartanshieldsunofficial/tags/ModItemTags.java
@@ -10,27 +10,27 @@ import net.minecraft.world.item.Item;
 
 public class ModItemTags 
 {
-	public static final TagKey<Item> TIN_INGOT = ItemTags.create(ResourceLocation.fromNamespaceAndPath("forge", "ingots/tin"));
-	public static final TagKey<Item> BRONZE_INGOT = ItemTags.create(ResourceLocation.fromNamespaceAndPath("forge", "ingots/bronze"));
-	public static final TagKey<Item> STEEL_INGOT = ItemTags.create(ResourceLocation.fromNamespaceAndPath("forge", "ingots/steel"));
-	public static final TagKey<Item> SILVER_INGOT = ItemTags.create(ResourceLocation.fromNamespaceAndPath("forge", "ingots/silver"));
-	public static final TagKey<Item> ELECTRUM_INGOT = ItemTags.create(ResourceLocation.fromNamespaceAndPath("forge", "ingots/electrum"));
-	public static final TagKey<Item> LEAD_INGOT = ItemTags.create(ResourceLocation.fromNamespaceAndPath("forge", "ingots/lead"));
-	public static final TagKey<Item> NICKEL_INGOT = ItemTags.create(ResourceLocation.fromNamespaceAndPath("forge", "ingots/nickel"));
-	public static final TagKey<Item> INVAR_INGOT = ItemTags.create(ResourceLocation.fromNamespaceAndPath("forge", "ingots/invar"));
-	public static final TagKey<Item> CONSTANTAN_INGOT = ItemTags.create(ResourceLocation.fromNamespaceAndPath("forge", "ingots/constantan"));
-	public static final TagKey<Item> PLATINUM_INGOT = ItemTags.create(ResourceLocation.fromNamespaceAndPath("forge", "ingots/platinum"));
-	public static final TagKey<Item> ALUMINUM_INGOT = ItemTags.create(ResourceLocation.fromNamespaceAndPath("forge", "ingots/aluminum"));
+	public static final TagKey<Item> TIN_INGOT = ItemTags.create(ResourceLocation.fromNamespaceAndPath("c", "ingots/tin"));
+	public static final TagKey<Item> BRONZE_INGOT = ItemTags.create(ResourceLocation.fromNamespaceAndPath("c", "ingots/bronze"));
+	public static final TagKey<Item> STEEL_INGOT = ItemTags.create(ResourceLocation.fromNamespaceAndPath("c", "ingots/steel"));
+	public static final TagKey<Item> SILVER_INGOT = ItemTags.create(ResourceLocation.fromNamespaceAndPath("c", "ingots/silver"));
+	public static final TagKey<Item> ELECTRUM_INGOT = ItemTags.create(ResourceLocation.fromNamespaceAndPath("c", "ingots/electrum"));
+	public static final TagKey<Item> LEAD_INGOT = ItemTags.create(ResourceLocation.fromNamespaceAndPath("c", "ingots/lead"));
+	public static final TagKey<Item> NICKEL_INGOT = ItemTags.create(ResourceLocation.fromNamespaceAndPath("c", "ingots/nickel"));
+	public static final TagKey<Item> INVAR_INGOT = ItemTags.create(ResourceLocation.fromNamespaceAndPath("c", "ingots/invar"));
+	public static final TagKey<Item> CONSTANTAN_INGOT = ItemTags.create(ResourceLocation.fromNamespaceAndPath("c", "ingots/constantan"));
+	public static final TagKey<Item> PLATINUM_INGOT = ItemTags.create(ResourceLocation.fromNamespaceAndPath("c", "ingots/platinum"));
+	public static final TagKey<Item> ALUMINUM_INGOT = ItemTags.create(ResourceLocation.fromNamespaceAndPath("c", "ingots/aluminum"));
 	
-	public static final TagKey<Item> MANASTEEL_INGOT = ItemTags.create(ResourceLocation.fromNamespaceAndPath("forge", "ingots/manasteel"));
-	public static final TagKey<Item> TERRASTEEL_INGOT = ItemTags.create(ResourceLocation.fromNamespaceAndPath("forge", "ingots/terrasteel"));
-	public static final TagKey<Item> ELEMENTIUM_INGOT = ItemTags.create(ResourceLocation.fromNamespaceAndPath("forge", "ingots/elementium"));
-	public static final TagKey<Item> OSMIUM_INGOT = ItemTags.create(ResourceLocation.fromNamespaceAndPath("forge", "ingots/osmium"));
-	public static final TagKey<Item> REFINED_GLOWSTONE_INGOT = ItemTags.create(ResourceLocation.fromNamespaceAndPath("forge", "ingots/refined_glowstone"));
-	public static final TagKey<Item> REFINED_OBSIDIAN_INGOT = ItemTags.create(ResourceLocation.fromNamespaceAndPath("forge", "ingots/refined_obsidian"));
-	public static final TagKey<Item> SIGNALUM_INGOT = ItemTags.create(ResourceLocation.fromNamespaceAndPath("forge", "ingots/signalum"));
-	public static final TagKey<Item> LUMIUM_INGOT = ItemTags.create(ResourceLocation.fromNamespaceAndPath("forge", "ingots/lumium"));
-	public static final TagKey<Item> ENDERIUM_INGOT = ItemTags.create(ResourceLocation.fromNamespaceAndPath("forge", "ingots/enderium"));
+	public static final TagKey<Item> MANASTEEL_INGOT = ItemTags.create(ResourceLocation.fromNamespaceAndPath("c", "ingots/manasteel"));
+	public static final TagKey<Item> TERRASTEEL_INGOT = ItemTags.create(ResourceLocation.fromNamespaceAndPath("c", "ingots/terrasteel"));
+	public static final TagKey<Item> ELEMENTIUM_INGOT = ItemTags.create(ResourceLocation.fromNamespaceAndPath("c", "ingots/elementium"));
+	public static final TagKey<Item> OSMIUM_INGOT = ItemTags.create(ResourceLocation.fromNamespaceAndPath("c", "ingots/osmium"));
+	public static final TagKey<Item> REFINED_GLOWSTONE_INGOT = ItemTags.create(ResourceLocation.fromNamespaceAndPath("c", "ingots/refined_glowstone"));
+	public static final TagKey<Item> REFINED_OBSIDIAN_INGOT = ItemTags.create(ResourceLocation.fromNamespaceAndPath("c", "ingots/refined_obsidian"));
+	public static final TagKey<Item> SIGNALUM_INGOT = ItemTags.create(ResourceLocation.fromNamespaceAndPath("c", "ingots/signalum"));
+	public static final TagKey<Item> LUMIUM_INGOT = ItemTags.create(ResourceLocation.fromNamespaceAndPath("c", "ingots/lumium"));
+	public static final TagKey<Item> ENDERIUM_INGOT = ItemTags.create(ResourceLocation.fromNamespaceAndPath("c", "ingots/enderium"));
 	
 	public static final TagKey<Item> BASIC_SHIELDS = ItemTags.create(ResourceLocation.fromNamespaceAndPath(ModSpartanShields.ID, "basic_shields"));
 	public static final TagKey<Item> TOWER_SHIELDS = ItemTags.create(ResourceLocation.fromNamespaceAndPath(ModSpartanShields.ID, "tower_shields"));
